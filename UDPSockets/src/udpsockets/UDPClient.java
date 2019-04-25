198756d86440af21f8c0a039370b7fad184f2231
kmiller-16 <kmille92@students.towson.edu>
2019-04-25 19:01:33 -0400

    Added TCP static functions

--- UDPSockets/src/udpsockets/UDPClient.java
+++ UDPSockets/src/udpsockets/UDPClient.java
@@ -1,30 +1,83 @@
 /*
+Instructions - Run the UDP Sockets file first to start the server. Then
+start the client. TCP will send mssgs back and forth between programs each side
+must say "bye" to end the connection. Once the TCP connection ends, UDP runs.
+UDP is just the client sending mssgs to the server. say "bye" from the client
+to quit. This code will have to be adapted to what he wants.
  */
 package udpsockets;
 //imports
+import java.io.BufferedReader;
 import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.DatagramPacket; 
 import java.net.DatagramSocket; 
 import java.net.InetAddress; 
-import java.util.Scanner; 
-/**
+import java.net.Socket;
 
- */
 
 public class UDPClient {
     public static void main(String args[]) throws IOException{
-        Scanner scan = new Scanner(System.in);
+        System.out.println(System.in.toString());
+        runTCPCli();
+        runUDPCli();
+    }
+    public static void runUDPCli() throws IOException{
         
+        System.out.println(System.in.toString());
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         DatagramSocket dSocket = new DatagramSocket();
         InetAddress ip = InetAddress.getLocalHost();
         byte buffer[] = null;
+        String input;
+        System.out.println("UDP Client Started...");
+        boolean displayed = false;
         while(true){
-            String input = scan.nextLine();
-            buffer = input.getBytes();
-            DatagramPacket sendMe =  new DatagramPacket(
-                    buffer, buffer.length, ip, 5432);
-            dSocket.send(sendMe);
-            if(input.equals("bye")) break;
+                displayed = false;
+                input = read.readLine();
+                System.out.println("Sending: " + input);
+                buffer = input.getBytes();
+                DatagramPacket sendMe = new DatagramPacket(
+                        buffer, buffer.length, ip, 5432);
+                dSocket.send(sendMe);
+                if (input.equals("bye")) {
+                    break;
+                }
+            
+            else if (!displayed) {
+                System.out.println("waiting");
+                displayed = true;
+            }
         }
+        read.close();
+        dSocket.close();
+    }
+    public static void runTCPCli() throws IOException{
+        BufferedReader sin;
+        PrintStream sout;
+        BufferedReader stdin;
+        try (Socket sk = new Socket("127.0.0.1",5000)) {
+            sin = new BufferedReader (new InputStreamReader(sk.getInputStream()));
+            sout = new PrintStream(sk.getOutputStream());
+            stdin = new BufferedReader (new InputStreamReader(System.in));
+            String s;
+            while (true)
+            {
+                System.out.print("Client : ");
+                s=stdin.readLine();
+                sout.println(s);
+                if (s.equalsIgnoreCase("Bye"))
+                {
+                    System.out.println("Connection ended by client");
+                    break;
+                }
+                s=sin.readLine();
+                System.out.print("Server : " +s+"\n");
+            }
+        }
+        /*sin.close();
+        sout.close();
+        stdin.close();*/
     }
 }
--- UDPSockets/src/udpsockets/UDPSockets.java
+++ UDPSockets/src/udpsockets/UDPSockets.java
@@ -3,11 +3,17 @@
  */
 package udpsockets;
 //Imports for server side socket
+import java.io.BufferedReader;
 import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.DatagramPacket; 
 import java.net.DatagramSocket; 
 import java.net.InetAddress; 
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException; 
+import java.util.Scanner;
 /**
  */
 public class UDPSockets {
@@ -16,24 +22,8 @@ public class UDPSockets {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        //socket will listen at port 5432
-       DatagramSocket uSocket = new DatagramSocket(5432);
-       byte[] receiver = new byte[65535];
-       
-       DatagramPacket dPacket = null;
-       while(true){
-           dPacket = new DatagramPacket(receiver, receiver.length);
-           
-           uSocket.receive(dPacket);
-           
-           System.out.println("Message from client: " + data(receiver));
-           
-           if(data(receiver).toString().equals("bye")){
-               System.out.println("Client closed connection... goodbye!");
-               break;
-           }
-           receiver = new byte[65535];
-       }
+        runTCP();
+        runUDP();
     }
     public static StringBuilder data(byte[] a) 
     { 
@@ -48,5 +38,55 @@ public class UDPSockets {
         } 
         return ret; 
     } 
-    
+    public static void runUDP() throws IOException{
+         //socket will listen at port 5432
+       DatagramSocket uSocket = new DatagramSocket(5432);
+       byte[] receiver = new byte[65535];
+       
+       DatagramPacket dPacket = null;
+       System.out.println("UDP server starting up...");
+       while(true){
+           dPacket = new DatagramPacket(receiver, receiver.length);
+           
+           uSocket.receive(dPacket);
+           
+           System.out.println("Message from client: " + data(receiver));
+           
+           if(data(receiver).toString().equals("bye")){
+               System.out.println("Client closed connection... goodbye!");
+               break;
+           }
+           receiver = new byte[65535];
+       }
+       uSocket.close();
+    }
+    public static void runTCP() throws IOException{
+        ServerSocket ss=new ServerSocket(5000);
+        Socket sk=ss.accept();
+        BufferedReader cin= new BufferedReader (new InputStreamReader(sk.getInputStream()));
+        PrintStream cout=new PrintStream(sk.getOutputStream());
+        BufferedReader stdin=new BufferedReader (new InputStreamReader(System.in));
+        String s;
+        Scanner sc=new Scanner(System.in);
+        while (true)
+        {
+            s=cin.readLine();
+            System.out.print("Client : "+s+"\n");
+            System.out.print("Server : ");
+            s=sc.nextLine();
+            //s=sout.println(s);
+            if (s.equalsIgnoreCase("bye"))
+            {
+                cout.println("Bye");
+                System.out.println("Connection ended by server");
+                break;
+            }
+            cout.println(s);
+        }
+        ss.close();
+        sk.close();
+        cin.close();
+        cout.close();
+        stdin.close();
+    }
 }
